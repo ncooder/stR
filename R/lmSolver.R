24f04381e44009e80e60ea7fb024bc8f94fcc517
Norbert Kuder <kudernorbert@gmail.com>
2025-02-02 23:50:21 +0100

    Removed env from lmSolver

--- R/lmSolver.R
+++ R/lmSolver.R
@@ -2,108 +2,116 @@
 # Environment env is used to pass some useful data
 # (such as last solution and Cholesky decomposition)
 # between different calls of lmSolver.
-lmSolver <- function(X, y, type = "Matrix", method = "cholesky", env = NULL, iterControl = list(maxiter = 20, tol = 1E-6), trace = TRUE) {
-  if (length(y) > nrow(X)) stop("y is too long in lmSolver...")
+lmSolver <- function(X,
+                     y,
+                     type = "Matrix",
+                     method = "cholesky",
+                     iterControl = list(maxiter = 20, tol = 1e-6),
+                     trace = TRUE) {
+  if (length(y) > nrow(X)) {
+    stop("y is too long in lmSolver...")
+  }
+
   y <- c(y, rep(0, nrow(X) - length(y)))
+
+  y <- as.numeric(y)
+
   if (type == "Matrix") {
     if (method == "cholesky") {
       result <- .solve.dgC.chol(t(X), y)
       return(result$coef)
     }
     if (method == "qr") {
-      b <- solve(qr(X), y) # Not optimal
+      b <- solve(qr(X), y)
       return(b)
     }
-    stop("Unknown method in lmSolver...")
+    stop("Unknown 'method' for type='Matrix' in lmSolver...")
   }
+
   if (type == "iterative") {
     if (method == "cg") {
-      f <- function(z) crossprod(X, X %*% z)
-      if (is.null(env) || is.null(env$L) || is.null(env$Lt) || is.null(env$P) || is.null(env$Pt)) {
-        invm <- 1 / colSums(X^2)
-        invf <- function(z) invm * z
-      } else {
-        invf <- function(z) solve(env$P, solve(env$Lt, solve(env$L, solve(env$Pt, z))))
-      }
+      f <- function(z) crossprod(X, as.numeric(X %*% z))
+      invm <- 1 / colSums(X^2)
+      invf <- function(z) invm * z
+
       Xty <- crossprod(X, y)
-      if (is.null(env) || is.null(env$b0)) {
-        b0 <- rep(0, length(Xty))
-      } else {
-        b0 <- env$b0
-      }
-      result <- olscg(FUN = f, y = Xty, b = b0, invFUN = invf, iterControl = iterControl, trace = trace)
+      b0 <- rep(0, length(Xty))
+
+      result <- olscg(FUN = f,
+                      y = Xty,
+                      b = b0,
+                      invFUN = invf,
+                      iterControl = iterControl,
+                      trace = trace)
       return(result$b)
     }
+
     if (method == "cg-chol") {
-      if (is.null(env)) {
-        stop("env variable is NULL.")
-      }
-      if (is.null(env$L) ||
-        is.null(env$Lt) ||
-        is.null(env$P) ||
-        is.null(env$Pt)) {
-        result <- .solve.dgC.chol(t(X), y)
-        eL <- Matrix::expand(result$L)
-        env$L <- eL$L
-        env$Lt <- t(eL$L)
-        env$P <- eL$P
-        env$Pt <- t(eL$P)
-        env$b0 <- result$coef
-        return(result$coef)
-      } else {
-        f <- function(z) crossprod(X, X %*% z)
-        invf <- function(z) solve(env$P, solve(env$Lt, solve(env$L, solve(env$Pt, z))))
-        Xty <- crossprod(X, y)
-        if (is.null(env$b0)) {
-          b0 <- rep(0, length(Xty))
-        } else {
-          b0 <- env$b0
-        }
-        result <- olscg(FUN = f, y = Xty, b = b0, invFUN = invf, iterControl = iterControl, trace = trace)
-        return(result$b)
-      }
+      cholResult <- .solve.dgC.chol(t(X), y)
+      eL <- Matrix::expand(cholResult$L)
+      L <- eL$L
+      Lt <- t(eL$L)
+      P <- eL$P
+      Pt <- t(eL$P)
+
+      f <- function(z) crossprod(X, as.numeric(X %*% z))
+      invf <- function(z) solve(P, solve(Lt, solve(L, solve(Pt, z))))
+
+      Xty <- crossprod(X, y)
+      b0 <- rep(0, length(Xty))
+
+      result <- olscg(FUN = f,
+                      y = Xty,
+                      b = b0,
+                      invFUN = invf,
+                      iterControl = iterControl,
+                      trace = trace)
+
+      return(as.numeric(result$b))
     }
+
     if (method == "lsmr-chol") {
-      if (is.null(env)) {
-        stop("env variable is NULL.")
-      }
-      if (is.null(env$L) ||
-        is.null(env$Lt) ||
-        is.null(env$P) ||
-        is.null(env$Pt)) {
-        result <- .solve.dgC.chol(t(X), y)
-        eL <- Matrix::expand(result$L)
-        env$L <- eL$L
-        env$Lt <- t(eL$L)
-        env$P <- eL$P
-        env$Pt <- t(eL$P)
-        env$b0 <- result$coef
-        return(result$coef)
-      } else {
-        A <- function(x, k) {
-          if (k == 1) {
-            return(X %*% solve(env$P, solve(env$Lt, x)))
-          } else {
-            return(solve(env$L, solve(env$Pt, crossprod(X, x))))
-          }
-        }
-        if (!is.null(env$b0)) {
-          x0 <- env$b0
+      cholResult <- .solve.dgC.chol(t(X), y)
+      eL <- Matrix::expand(cholResult$L)
+      L <- eL$L
+      Lt <- t(L)
+      P <- eL$P
+      Pt <- t(P)
+
+      invL <- function(x) as.numeric(solve(L, x, system = "L"))
+      invLt <- function(x) as.numeric(solve(Lt, x, system = "U"))
+
+      invP <- function(x) as.numeric(t(P) %*% x)
+      invPt <- function(x) as.numeric(t(Pt) %*% x)
+
+      A <- function(x, k) {
+        if (k == 1) {
+          return(X %*% invP(invLt(x)))
         } else {
-          x0 <- 0
+          tmp <- crossprod(X, x)
+          tmp <- invPt(tmp)
+          return(invL(tmp))
         }
-        result <- lsmr(A = A, b = y - X %*% x0, atol = iterControl$tol, btol = iterControl$tol, itnlim = iterControl$maxiter)
-        if (trace) {
-          cat("\nIterations: ")
-          cat(result$itn)
-          cat("   ")
-        }
-        return(solve(env$P, solve(env$Lt, result$x)) + x0)
       }
+
+      x0 <- 0
+      bVec <- y
+
+      result <- lsmr(A = A,
+                     b = bVec,
+                     atol = iterControl$tol,
+                     btol = iterControl$tol,
+                     itnlim = iterControl$maxiter)
+
+      tmp <- invLt(result$x)
+      tmp <- invP(tmp)
+      return(as.numeric(tmp + x0))
     }
+
     if (method == "lsmr") {
       D <- sqrt(colSums(X^2))
       invD <- 1 / D
+
       A <- function(x, k) {
         if (k == 1) {
           return(X %*% (invD * x))
@@ -111,24 +119,25 @@ lmSolver <- function(X, y, type = "Matrix", method = "cholesky", env = NULL, ite
           return(invD * crossprod(X, x))
         }
       }
-      if (!is.null(env$b0)) {
-        x0 <- env$b0
-      } else {
-        x0 <- rep(0, ncol(X))
-      }
-      result <- lsmr(A = A, b = y - X %*% x0, atol = iterControl$tol, btol = iterControl$tol, itnlim = iterControl$maxiter)
+
+      x0 <- rep(0, ncol(X))
+      bVec <- y
+
+      result <- lsmr(A = A,
+                     b = bVec,
+                     atol = iterControl$tol,
+                     btol = iterControl$tol,
+                     itnlim = iterControl$maxiter)
+
       if (trace) {
-        cat("\nIterations: ")
-        cat(result$itn)
-        cat("   ")
+        cat("\nIterations:", result$itn, "  ")
       }
-      output <- invD * result$x + x0
-      if (!is.null(env)) {
-        env$b0 <- output
-      }
-      return(output)
+      return(invD * result$x + x0)
     }
-    stop("Unknown method in lmSolver...")
+
+    stop("Unknown 'method' for type='iterative' in lmSolver...")
   }
+
   stop("Unknown type in lmSolver...")
 }
+
